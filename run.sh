e94daaba023759760caf4170878da97be0dc7b6a
jayshah <reversearrow@gmail.com>
2024-01-27 16:11:17 -0500

    combine two jobs into one

--- run.sh
+++ run.sh
@@ -7,12 +7,6 @@ if [ -z "$SERVICE_NAME" ]; then
   exit 1
 fi
 
-RELEASE_VERSION=$2
-if [ -z "$RELEASE_VERSION" ]; then
-  echo "Error: RELEASE_VERSION is not set."
-  exit 1
-fi
-
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
@@ -22,6 +16,4 @@ go mod init github.com/reversearrow/microservices-proto/golang/"${SERVICE_NAME}"
 git config --global user.email "reversearrow@gmail.com"
 git config --global user.name "Jay Shah"
 git add . && git commit -am "proto update" || true
-git tag -fa golang/"${SERVICE_NAME}"/"${RELEASE_VERSION}" \
--m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
-git push origin refs/tags/golang/"${SERVICE_NAME}"/"${RELEASE_VERSION}"
+git push origin master
